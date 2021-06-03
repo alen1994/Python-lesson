2e8718779f6fbffc9dfb8c7fe2182bd100f8f8fc
alen1994 <39806926+alen1994@users.noreply.github.com>
2021-06-03 19:04:00 +0200

    Update naloga1.py

---
+++ naloga1.py
@@ -0,0 +1,15 @@
+mood = input("Vpiši svoje počutje: ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervos":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Take some shots! =D")
+elif mood == "excited":
+    print("You GO girl!")
+elif mood == "relaxed":
+    print("Sunshine, sunshine reggae...")
+else:
+    print("I don't recognize this mood")
+
