dd560539c37de398b5e6edd19f30d19f29a2fe55
alen1994 <39806926+alen1994@users.noreply.github.com>
2021-06-03 19:04:27 +0200

    Update naloga2.py

---
+++ naloga2.py
@@ -0,0 +1,7 @@
+secret = 14
+guess = int(input("Zadeni skrivnostno številko od 1-20: "))
+
+if guess == secret:
+    print("Čestitke zadeli ste! =D")
+else:
+    print("Niste zadeli skrivnostne številke =(")
